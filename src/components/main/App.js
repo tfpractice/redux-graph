6d1dbecd53d30cfc19adaf4545a2af6b05e3a98f
theory friction practice <jackson.brebnor@gmail.com>
2017-02-02 12:08:41 -0500

    creates reducer and store files

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,19 +1,26 @@
 import React, { Component, } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme, { userAgent: false, })}>
+
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        
+      </MuiThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components';
+import { Provider, } from 'react-redux';
+import { App, } from './components';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
