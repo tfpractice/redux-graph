24d12d1388141248caf6dadc3cdd8d52a487e3bf
theory friction practice <jackson.brebnor@gmail.com>
2017-02-02 12:40:54 -0500

    renamed edges module to graph

---
+++ src/modules/graph/constants.js
@@ -0,0 +1,35 @@
+export const ADD_EDGES = 'ADD_EDGES';
+export const ADD_ENTRY = 'ADD_ENTRY';
+export const ADD_NEIGHBOR = 'ADD_NEIGHBOR';
+export const ADD_NODES = 'ADD_NODES';
+export const ADJ = 'ADJ';
+export const CONTAINS = 'CONTAINS';
+export const COPY = 'COPY';
+export const FROM_ELEMENTS = 'FROM_ELEMENTS';
+export const IS_ADJACENT = 'IS_ADJACENT';
+export const MERGE_EDGES = 'MERGE_EDGES';
+export const NEIGHBORS = 'NEIGHBORS';
+export const NODES = 'NODES';
+export const REMOVE_EDGES = 'REMOVE_EDGES';
+export const REMOVE_NODES = 'REMOVE_NODES';
+export const RESET_NODES = 'RESET_NODES';
+export const SPAWN = 'SPAWN';
+
+export const GRAPH_ACTIONS = new Set([
+  ADD_EDGES,
+  ADD_ENTRY,
+  ADD_NEIGHBOR,
+  ADD_NODES,
+  ADJ,
+  CONTAINS,
+  COPY,
+  FROM_ELEMENTS,
+  IS_ADJACENT,
+  MERGE_EDGES,
+  NEIGHBORS,
+  NODES,
+  REMOVE_EDGES,
+  REMOVE_NODES,
+  RESET_NODES,
+  SPAWN,
+]);
