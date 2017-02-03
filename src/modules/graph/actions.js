0bac91187a4ada805e8318be9fe4c9979da90a9d
theory friction practice <jackson.brebnor@gmail.com>
2017-02-03 12:46:19 -0500

    refactors gActions

--- src/modules/graph/actions.js
+++ src/modules/graph/actions.js
@@ -5,8 +5,8 @@ import { ADD_EDGES, ADD_NODES, FROM_ELEMENTS, MERGE_EDGES, NEIGHBORS, NODES,
 // const createBin = (state, v) => state.concat(state.length);
 // const create = (count = 1) => state => Array(count).fill(1).reduce(createBin, state);
 
-const addEdgesPermute = (src, w = 0) => (...nabes) => edges =>
-Graph.addEdges(edges)(src, w)(...nabes);
+const addEdgesPermute = (src, w = 0) => (...nabes) => g =>
+Graph.addEdges(g)(src, w)(...nabes);
 
 export const addEdges = (s, w) => dispatch => (...nabes) =>
  ({ type: ADD_EDGES, curry: addEdgesPermute(s, w)(...nabes), });
@@ -14,58 +14,58 @@ export const addEdges = (s, w) => dispatch => (...nabes) =>
 // export const addEntryPermute = nabes => ([ n, w = 0 ]) => addBinMap(nabes, [ n, w ]);
 // export const addEntry = nabes => ([ n, w = 0 ]) => addBinMap(nabes, [ n, w ]);
 
-// export const addNeighborPermute = edges => src => (n, w = 0) =>
-// export const addNeighbor = edges => src => (n, w = 0) =>
+// export const addNeighborPermute = g => src => (n, w = 0) =>
+// export const addNeighbor = g => src => (n, w = 0) =>
 
-const addNodesPermute = (...srcs) => edges => Graph.addNodes(edges)(srcs);
+const addNodesPermute = (...srcs) => g => Graph.addNodes(g)(srcs);
 
 export const addNodes = (...srcs) =>
 ({ type: ADD_NODES, curry: addNodesPermute(...srcs), });
 
-// export const adjPermute = edges => src => asMap(get(edges)(src));
-// export const adj = edges => src => asMap(get(edges)(src));
+// export const adjPermute = g => src => asMap(get(g)(src));
+// export const adj = g => src => asMap(get(g)(src));
 
-// export const containsPermute = edges => node => hasK(edges)(node);
-// export const contains = edges => node => hasK(edges)(node);
+// export const containsPermute = g => node => hasK(g)(node);
+// export const contains = g => node => hasK(g)(node);
 
 // export const copyPermute = spawn;
 // export const copy = spawn;
 
-// export const disconnectNodesPermute = edges => (...srcs) =>
-// export const disconnectNodes = edges => (...srcs) =>
+// export const disconnectNodesPermute = g => (...srcs) =>
+// export const disconnectNodes = g => (...srcs) =>
 
 // export const fromElementsPermute = (...elems) => elems.reduce(addNodeBin, copy());
 // export const fromElements = (...elems) => elems.reduce(addNodeBin, copy());
 
-// export const isAdjacentPermute = edges => src => nb => contains(adj(edges)(src))(nb);
-// export const isAdjacent = edges => src => nb => contains(adj(edges)(src))(nb);
+// export const isAdjacentPermute = g => src => nb => contains(adj(g)(src))(nb);
+// export const isAdjacent = g => src => nb => contains(adj(g)(src))(nb);
 
-export const mergeEdgesPermute = (...alt) => edges =>
- Graph.mergeEdges(edges)(...alt);
+const mergeEdgesPermute = (...alt) => g => Graph.mergeEdges(g)(...alt);
+
 export const mergeEdges = (...alt) =>
 ({ type: MERGE_EDGES, curry: mergeEdgesPermute(...alt), });
 
 // export const mergeNeighborsPermute = uniteMap;
 // export const mergeNeighbors = uniteMap;
 
-// export const neighborsPermute = edges => src => nodes(adj(edges)(src));
-// export const neighbors = edges => src => nodes(adj(edges)(src));
+// export const neighborsPermute = g => src => nodes(adj(g)(src));
+// export const neighbors = g => src => nodes(adj(g)(src));
 
-// export const nodesPermute = edges => spreadK(copy(edges));
-// export const nodes = edges => spreadK(copy(edges));
+// export const nodesPermute = g => spreadK(copy(g));
+// export const nodes = g => spreadK(copy(g));
 
-export const removeEdgesPermute = src => (...nabes) => edges =>
-Graph.removeEdges(edges)(src)(...nabes);
-export const removeEdges = src => (...nabes) => edges =>
+const removeEdgesPermute = src => (...nabes) => g => Graph.removeEdges(g)(src)(...nabes);
+
+export const removeEdges = src => (...nabes) => g =>
 ({ type: REMOVE_EDGES, curry: removeEdgesPermute(src)(...nabes), });
 
-export const removeNodesPermute = (...srcs) => edges =>
- Graph.removeNodes(...srcs)(edges);
+const removeNodesPermute = (...srcs) => g => Graph.removeNodes(...srcs)(g);
+
 export const removeNodes = (...srcs) =>
 ({ type: REMOVE_NODES, curry: removeNodesPermute(...srcs), });
 
-// export const resetNodesPermute = edges => (...src) => src.reduce(resetNodeBin, edges);
-// export const resetNodes = edges => (...src) => src.reduce(resetNodeBin, edges);
+// export const resetNodesPermute = g => (...src) => src.reduce(resetNodeBin, edges);
+// export const resetNodes = g => (...src) => src.reduce(resetNodeBin, edges);
 
-// export const spawnPermute = edges => asMap(edges);
-// export const spawn = edges => asMap(edges);
+// export const spawnPermute = g => asMap(g);
+// export const spawn = g => asMap(g);
